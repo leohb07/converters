76497c68362ea4885c847b06e6c35438e5a54ae3
leohb07 <leohb1501@gmail.com>
2022-05-10 22:15:25 -0300

    Iniciando projeto e desenhando layout

---
+++ Converters/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Converters
+//
+//  Created by Leonardo Henrique Barrocal on 10/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var valueTextField: UITextField!
+  
+  @IBOutlet weak var unitOneButton: UIButton!
+  @IBOutlet weak var unitTwoButton: UIButton!
+  
+  @IBOutlet weak var resultLabel: UILabel!
+  @IBOutlet weak var resultUnitLabel: UILabel!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func tappedShowNextButton(_ sender: UIButton) {
+    
+  }
+  
+  
+  @IBAction func tappedConvertButton(_ sender: UIButton) {
+    
+  }
+  
+}
+
